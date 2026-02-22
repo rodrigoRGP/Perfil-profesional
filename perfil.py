2b0ccdb6ed1ac5ca3afda46a36226f31959f2eea
Rodrigo <rodrigo-gp95@hotmail.com>
2026-02-21 20:30:45 -0600

    Agrego mi perfil profesional básico

---
+++ perfil.py
@@ -0,0 +1,9 @@
+nombre = "Rodrigo"
+stack = ["Python", "VS Code", "Git"]
+contacto = {
+    "email": "Rodrigo-gp95@hotmail.com",
+    "telefono": "5578218631"
+}
+print(nombre)
+print(stack)
+print(contacto)
